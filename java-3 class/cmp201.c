aac2f59ba04767a50ecf47282eb5f063d18fe932
adeoluwa31 <133676289+adeoluwa31@users.noreply.github.com>
2024-05-23 21:45:47 +0100

    Add files via upload

---
+++ java-3 class/cmp201.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+int main() {
+
+char operator = '+';
+
+int num1 = 8;
+int num2 = 7;
+
+int result = operator == '+' ? num1 + num2 : num1 - num2;
+printf("%d", result);
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+#include <stdio.h>
+
+int main() {
+
+    printf("what colour is the ball selected");
+
+    int totalballs = 100;
+    float ballWeight; 
+    int numberOfballsA, numberOfballsB, totalBallsboxA, totalBallsboxB;
+    char C; //colour of the ball selected
+
+    while (totalballs < 0){
+          
+    }
+    
+
+
+
+    return 0;
+    */
